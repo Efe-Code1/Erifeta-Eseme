7b3fad0e7df95e480cf131fab814a98279ffe8cf
Efe-code1 <efezzy321@gmail.com>
2024-03-07 18:51:58 +0100

    Affirmed

--- portfolio project.js
+++ portfolio project.js
@@ -22,86 +22,86 @@ pfts.forEach(pft => {
     })
 })
 
-let par = document.querySelector('.parent')
-let pos = 0
+// let par = document.querySelector('.parent')
+// let pos = 0
 
-setInterval(() => {
-    par.style.transform = `rotateY(${pos}deg)`
-    pos += 1
-}, 16.67)
+// setInterval(() => {
+//     par.style.transform = `rotateY(${pos}deg)`
+//     pos += 1
+// }, 16.67)
 
-let h4 = document.getElementsByTagName('h4')[0]
-let str = 'A Certified Geophysicist'
-let len = str.length
-let index = 0
+// let h4 = document.getElementsByTagName('h4')[0]
+// let str = 'A Certified Geophysicist'
+// let len = str.length
+// let index = 0
 
-let h4b = document.getElementsByTagName('h4')[1]
-let str2 = 'Tech Savvy &'
-let len2 = str2.length
-let index2 = 0
+// let h4b = document.getElementsByTagName('h4')[1]
+// let str2 = 'Tech Savvy &'
+// let len2 = str2.length
+// let index2 = 0
 
-let h4c = document.getElementsByTagName('h4')[2]
-let str3 = 'A Proficient Web Developer'
-let len3 = str3.length
-let index3 = 0
+// let h4c = document.getElementsByTagName('h4')[2]
+// let str3 = 'A Proficient Web Developer'
+// let len3 = str3.length
+// let index3 = 0
 
-let PERIODA = 90 //MILLISECONDS
+// let PERIODA = 90 //MILLISECONDS
 
-setTimeout(() => {
-    let handle = setInterval(() => {
-        if (index >= len) {
-            clearInterval(handle)
-            startB()
-            let strB = h4.innerHTML.replace('<span>|</span>', '')
-            h4.innerHTML = strB
-        } else {
-            let strB = h4.innerHTML.replace('<span>|</span>', '')
-            h4.innerHTML = strB
-            h4.innerHTML += str[index] + '<span>|</span>'
-            index += 1
-        }
-    }, PERIODA)
-}, 1900)
+// setTimeout(() => {
+//     let handle = setInterval(() => {
+//         if (index >= len) {
+//             clearInterval(handle)
+//             startB()
+//             let strB = h4.innerHTML.replace('<span>|</span>', '')
+//             h4.innerHTML = strB
+//         } else {
+//             let strB = h4.innerHTML.replace('<span>|</span>', '')
+//             h4.innerHTML = strB
+//             h4.innerHTML += str[index] + '<span>|</span>'
+//             index += 1
+//         }
+//     }, PERIODA)
+// }, 1900)
 
-function startB() {
-    let handleB = setInterval(() => {
-        if (index2 >= len2) {
-            clearInterval(handleB)
-            startC()
-            let strB = h4b.innerHTML.replace(/<span>\|<\/span>/g, '')
-            h4b.innerHTML = strB
-        } else {
-            let strB = h4b.innerHTML.replace('<span>|</span>', '')
-            h4b.innerHTML = strB
-            h4b.innerHTML += str2[index2] + '<span>|</span>'
-            index2 += 1
-        }
-    }, PERIODA)
-}
+// function startB() {
+//     let handleB = setInterval(() => {
+//         if (index2 >= len2) {
+//             clearInterval(handleB)
+//             startC()
+//             let strB = h4b.innerHTML.replace(/<span>\|<\/span>/g, '')
+//             h4b.innerHTML = strB
+//         } else {
+//             let strB = h4b.innerHTML.replace('<span>|</span>', '')
+//             h4b.innerHTML = strB
+//             h4b.innerHTML += str2[index2] + '<span>|</span>'
+//             index2 += 1
+//         }
+//     }, PERIODA)
+// }
 
-function startC() {
-    let handleC = setInterval(() => {
-        if (index3 >= len3) {
-            clearInterval(handleC)
-            startD()
-        } else {
-            let strB = h4c.innerHTML.replace('<span>|</span>', '')
-            h4c.innerHTML = strB
-            h4c.innerHTML += str3[index3] + '<span>|</span>'
-            index3 += 1
-        }
-    }, PERIODA)
-}
-function startD() {
-    setInterval(() => {
-        let span3 = document.getElementsByTagName('span')[5];
-        switch (parseInt(window.getComputedStyle(span3).getPropertyValue('opacity'))) {
-            case 0:
-                // span3.style.opacity = 1
-                // break
-            case 1:
-                span3.style.opacity = 0
-                break
-        }
-    }, 400)
-}
+// function startC() {
+//     let handleC = setInterval(() => {
+//         if (index3 >= len3) {
+//             clearInterval(handleC)
+//             startD()
+//         } else {
+//             let strB = h4c.innerHTML.replace('<span>|</span>', '')
+//             h4c.innerHTML = strB
+//             h4c.innerHTML += str3[index3] + '<span>|</span>'
+//             index3 += 1
+//         }
+//     }, PERIODA)
+// }
+// function startD() {
+//     setInterval(() => {
+//         let span3 = document.getElementsByTagName('span')[5];
+//         switch (parseInt(window.getComputedStyle(span3).getPropertyValue('opacity'))) {
+//             case 0:
+//                 // span3.style.opacity = 1
+//                 // break
+//             case 1:
+//                 span3.style.opacity = 0
+//                 break
+//         }
+//     }, 400)
+// }
